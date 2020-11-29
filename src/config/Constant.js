295fe4bd887fe8f37276e4893d6b782f41068502
Umer Fareed <umerfareed72@gmail.com>
2020-11-29 18:09:56 +0500

    c

--- src/config/Constant.js
+++ src/config/Constant.js
@@ -40,7 +40,7 @@ export const screenWidth = {
 
 //Url
 export const URL = {
-  Url: 'http://192.168.43.84:5000/',
+  Url: 'http://192.168.0.105:5000/',
 };
 //colors
 export const colors = {
--- src/server/models/uterms.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const uterms = new mongoose.Schema({
-    uterms:String,
-});
-
-mongoose.model('uterms', uterms);
